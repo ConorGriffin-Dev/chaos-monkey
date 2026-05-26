8984c87ffb608f8801855ce9562ef28902f5d3e5
ConorGriffin-Dev <conorgriffin222@gmail.com>
2026-05-26 21:29:32 +0100

    chore: add spring boot project scaffold from initialiser

---
+++ src/test/java/chaos_monkey/ChaosMonkeyApplicationTests.java
@@ -0,0 +1,13 @@
+package chaos_monkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaosMonkeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
